1a74724b7e549308abbe62215d2db579d98699ef
kaminuma <>
2025-08-29 01:33:09 +0900

    ver up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,8 +113,8 @@ android {
         applicationId = "com.sbm.application"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
